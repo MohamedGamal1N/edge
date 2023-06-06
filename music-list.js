6ba700771e5a03c7a587eee514873d38ab4bdae8
Mohamed-Gamal <98858516+MohamedGamal1N@users.noreply.github.com>
2023-06-07 00:38:34 +0300

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,68 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ana feeha ya bny iCon",
+    artist: "Mohamed Edge",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Vodka",
+    artist: "Mohamed Edge",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Ana shab zay elghool",
+    artist: "Mohamed Edge",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Alm w korasa",
+    artist: "Mohamed Edge",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Hala",
+    artist: "Mohamed Edge",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Badeel",
+    artist: "Mohamed Edge",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Pinocchio",
+    artist: "Mohamed Edge",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Harb",
+    artist: "Mohamed Edge",
+    img: "music-8",
+    src: "music-8"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
